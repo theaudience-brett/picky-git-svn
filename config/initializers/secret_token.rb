5669afb521d714dca2034f030dabf1a3f6c915f9
Brett Mack <mack.brett@gmail.com>
2013-02-08 20:20:51 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PickyGitSvn::Application.config.secret_token = '6305619bda56fe8bdf682a9b5239f8e16d0216f8595acb25189f3741ed9fdaa7ce72db097d731af52180b241d47595cfea0c78f75036829bf4b3865f490afb5e'
